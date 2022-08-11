d2b8825436e7db705623f75a6e49b2eceec05ae2
vlad <vladpuschiulescu@gmail.com>
2022-08-11 21:40:35 +0300

    Implement page routing

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {darkTheme, GlobalStyles, lightTheme, ThemeContext} from "./components/ThemeContext";
+import * as client from "./components/Cookies"
+import {ThemeProvider} from "styled-components";
+import {Route, Routes} from "react-router-dom";
+import RestrictVisitorToLandingPage from "./components/RestrictVisitorToLandingPage";
+import Navbar from "./components/Navbar";
+import SocialBar from "./components/LandingPage/SocialBar";
+
 
 function App() {
+
+  const [theme, setTheme] = useState(client.getCookie("theme") ? client.getCookie("theme") : "light")
+
   return (
     <>
-
+      <ThemeContext.Provider value={ {theme, setTheme} }>
+          <ThemeProvider theme={ theme === "light" ? lightTheme : darkTheme }>
+              <GlobalStyles themeObject={ theme === "light" ? lightTheme : darkTheme } />
+              <Routes>
+                  <Route element={ <RestrictVisitorToLandingPage/> }>
+                      <Route path={"/"} element={
+                          <>
+                              <SocialBar/>
+                          </>
+                      }
+                      />
+                  </Route>
+              </Routes>
+          </ThemeProvider>
+      </ThemeContext.Provider>
     </>
   );
 }
